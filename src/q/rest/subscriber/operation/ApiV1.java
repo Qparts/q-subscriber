07db0d6c13a307c223a532cb3164482e3dbf975a
fareed <fareed@qetaa.com>
2021-03-12 11:07:14 +0300

    fixed sql

--- src/q/rest/subscriber/operation/ApiV1.java
+++ src/q/rest/subscriber/operation/ApiV1.java
@@ -531,7 +531,7 @@ public class ApiV1 {
         }
         sql = "select to_char(z.date, 'Mon') as mon," +
                 " extract(year from z.date) as yy," +
-                " count (z.*) as count" +
+                " z.count as count" +
                 " from" +
                 " (select date_trunc('month', created) as date, count(*) as count" +
                 " from sub_replacement_search_keyword GROUP BY date_trunc('month', created) order by date desc limit 6)z order by z.date;";
